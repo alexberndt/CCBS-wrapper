3ad67c4d00417f4f605995b3e249f889a6610e7e
Alexander Berndt <alexberndt7@gmail.com>
2020-05-09 22:02:41 +0200

    Basic wrapper done

--- main.py
+++ main.py
@@ -4,17 +4,27 @@ import os
 import subprocess
 import xml.etree.cElementTree as ET
 from xml.dom import minidom
+import xmltodict
 
 class PlanItem(object):
-    def __init__(self, robot_id, t, x, y, vertex, yaw=0):
+    def __init__(self, robot_id, t_s, start, t_g, goal, yaw=0):
         self.robot_id   = robot_id
-        self.t          = time 
-        self.x          = x
-        self.y          = y 
-        self.yaw        = yaw 
-        self.vertex     = vertex
+        self.t_s        = t_s 
+        self.start      = start     # {"x": 8, "y": 9}
+        self.t_g        = t_g
+        self.goal       = goal      # {"x": 8, "y": 9}
 
-class GlobalPlan(object):
+    def get_info(self):
+        info = "\t--------------- \n"
+        info += "\tt_s: {} \n".format(self.t_s)
+        info += "\tstart: {} \n".format(self.start)
+        info += "\tt_g: {} \n".format(self.t_g)
+        info += "\tgoal: {} \n".format(self.goal)
+        info += "\t---------------"
+        return info
+        
+
+class CCBS_Planner(object):
     def __init__(self, map_file, goal_locations):
         self.map_file   = map_file # local reference to a map file
         self.pwd = os.path.dirname(os.path.realpath(__file__))
@@ -22,13 +32,13 @@ class GlobalPlan(object):
         # DEFINE FILE LOCATIONS
         self.map_dir    = self.pwd + "/maps/"
         self.task_dir   = self.pwd + "/tasks/"
-        self.ccbs_dir   = self.pwd + "/CCBS_graphs/"
+        self.ccbs_dir   = self.pwd + "/CCBS/"
 
         self.goal_locations = goal_locations # List of goal locations
 
         self.task_cnt   = 0
 
-    def run_task(self, task_description):
+    def solve(self, task_description):
         
         # task_description
         # Dictionary of AGVs with start and goal positions { 0: {"start": 1, "goal": 3}, 1: {"start": 2, "goal": 5} }
@@ -46,9 +56,67 @@ class GlobalPlan(object):
                 check=True)
         except:
             pass
+
+        global_plan = {}
  
         # READ THE TASK_LOG FILE
-        return self.task_log_file
+        print("opening task log file: {}".format(self.task_log_file))
+
+        with open(self.task_dir + self.task_log_file) as fd:
+            task_log = xmltodict.parse(fd.read())
+        # 1) Create vertices and Type 1 dependencies
+        for agent in task_log["root"]["log"]["agent"]:
+            _id = int(agent["@number"])
+            _plan = agent["path"]["hplevel"]["section"]
+            
+            global_plan[_id] = []
+
+            cumulative_length = 0.0
+            for action in _plan:
+
+                start   = {"x": float(action["@start.x"]), "y": float(action["@start.y"])}
+                goal    = {"x": float(action["@finish.x"]),"y": float(action["@finish.y"])}
+                length  = float(action["@length"]) # length = time to stay here
+                t_s     = cumulative_length
+                cumulative_length += length
+                t_g     = cumulative_length
+
+                # Only add if no start/goal
+                if not (start == goal and t_s == t_g):
+                    plan_item = PlanItem(_id, t_s, start, t_g, goal)
+                    global_plan[_id].append(plan_item)
+            
+            # self.vertices[_id] = []
+            # cumulative_length = 0
+            # v_no = 0    # vertex number
+            # _prev_vertex = None
+            # for action in _plan:
+            #     # extract data
+            #     start   = {"x": float(action["@start.x"]), "y": float(action["@start.y"])}
+            #     goal    = {"x": float(action["@finish.x"]),"y": float(action["@finish.y"])}
+            #     length  = float(action["@length"]) # length = time to stay here
+            #     t_s     = cumulative_length
+            #     cumulative_length += length
+            #     t_g     = cumulative_length
+            #     # if start and finish are the same, do not create ADG vertex
+            #     eps = 0.001
+            #     if (abs(start["x"] - goal["x"]) < eps and abs(start["y"] - goal["y"]) < eps):
+            #         # do not create new vertex
+            #         continue
+            #     # create new vertex
+            #     _vertex = Vertex(_id, v_no, t_s, t_g, start, goal, Status.STAGED)
+            #     v_no += 1
+            #     self.vertices[_id].append(_vertex)
+            #     # Add Type 1 dependencies
+            #     if _prev_vertex is not None:
+            #         self._add_edge(_prev_vertex, _vertex)
+            #     _prev_vertex = _vertex
+            
+            # init AGVs
+            # self.AGVs[_id] = Agv(_id, self.vertices[_id])
+            # self.AGVcnt += 1
+
+        return global_plan
 
     def get_map(self):
         print("get_map called!")
@@ -61,6 +129,7 @@ class GlobalPlan(object):
 
         self.task_cnt += 1
         task_file_name = "task_" + str(self.task_cnt) + ".xml"
+        print("task_file_name: {}".format(task_file_name))
 
         et = ET.parse(self.task_dir + "task_empty.xml")
         
@@ -83,24 +152,43 @@ class GlobalPlan(object):
     def _get_map_details(self):
         print("_get_map_details")
 
-if __name__ == "__main__":
 
-    global_plan     = GlobalPlan("map.xml", [])
-    global_map      = global_plan.get_map()
-    goal_locations  = global_plan.get_goal_locations()
+def main():
+    ccbs_planner    = CCBS_Planner("map.xml", [])
+    global_map      = ccbs_planner.get_map()
+    goal_locations  = ccbs_planner.get_goal_locations()
 
     # From the available goal locations, 
     # define a task for each AGV
-    
     task_defintion  = {0: {"start": 1, "goal": 3}, 
                        1: {"start": 2, "goal": 5}}
 
     # run the global plan
-    global_plan.run_task(task_defintion)
+    global_plan     = ccbs_planner.solve(task_defintion)
+
+    print("Global plan")
+    for robot_id in global_plan:
+        print("-----------------------------")
+        print("Robot ID: {}".format(robot_id))
+        for planitem in global_plan[robot_id]:
+            print(planitem.get_info())
 
     # set a new task definition
     task_defintion  = {0: {"start": 5,  "goal": 1}, 
                        1: {"start": 15, "goal": 2}}
 
     # run the global plan again
-    global_plan.run_task(task_defintion)
+    global_plan     = ccbs_planner.solve(task_defintion)
+
+    print("Global plan")
+    for robot_id in global_plan:
+        print("-----------------------------")
+        print("Robot ID: {}".format(robot_id))
+        for planitem in global_plan[robot_id]:
+            print(planitem.get_info())
+
+    
+
+
+if __name__ == "__main__":
+    main()
